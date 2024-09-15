98f0f75021b4f2f2ecab4473064cf16f29123b4a
Nick Fallon <nicholas.p.fallon@gmail.com>
2024-09-15 00:01:46 -0700

    style: install Sass and add theme colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,20 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'sf-black': '#171f1d',
+      'sf-gray': '#403D3E',
+      'sf-light-gray': '#a9a9a9', // lighter shade for backgrounds
+      'sf-dark-gray': '#2e2e2e', // darker shade for text or borders
+      'sf-white': '#eaecf0',
+      'sf-pink': '#FE65C3',
+      'sf-pink-light': '#FF8EDF', // lighter pink
+      'sf-pink-dark': '#D14E9B', // darker pink
+      'sf-purple': '#A263D8',
+      'sf-purple-light': '#B98BE8', // lighter purple
+      'sf-purple-dark': '#814BB4', // darker purple
+
+    },
     extend: {
       colors: {
         background: "var(--background)",
